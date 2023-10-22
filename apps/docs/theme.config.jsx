005789bc33a2af31487ea429ca49a684caf6ac2f
Yash Mehta <yashmehta0395@gmail.com>
2023-10-22 12:44:14 +0530

    docs added

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+	docsRepositoryBase: "https://github.com/planetgrow/planetgrow.site/tree/main/docs",
+	logo: <span>PlanetGrow Documentation</span>,
+	project: {
+		link: "https://github.com/planetgrow/planetgrow.site",
+	},
+	footer: false,
+	// ... other theme options
+};
